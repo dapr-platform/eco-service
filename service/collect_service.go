bc4c3543754c112c416581d8c65625b81d83979b
lyl <liuyulong@defensedriver.com.cn>
2024-11-09 15:18:49 +0800

    删除：     api/test.go

--- service/collect_service.go
+++ service/collect_service.go
@@ -12,12 +12,13 @@ import (
 	"github.com/dapr-platform/common"
 	"github.com/pkg/errors"
 )
+
 var needRefreshContinuousAggregateMap = map[string]string{
 	"f_eco_gateway_1d":  "day",
 	"f_eco_gateway_1m":  "month",
 	"f_eco_gateway_1y":  "year",
 	"f_eco_floor_1d":    "day",
-	"f_eco_floor_1m":    "month", 
+	"f_eco_floor_1m":    "month",
 	"f_eco_floor_1y":    "year",
 	"f_eco_building_1d": "day",
 	"f_eco_building_1m": "month",
@@ -33,31 +34,171 @@ func init() {
 			next := time.Date(now.Year(), now.Month(), now.Day(), now.Hour()+1, 5, 0, 0, now.Location())
 			time.Sleep(next.Sub(now))
 
-			if err := CollectGatewayHourlyStats(); err != nil {
+			gateways, err := GetAllEcgateways()
+			if err != nil {
+				common.Logger.Errorf("Failed to get gateways: %v", err)
+				continue
+			}
+
+			if err := collectGatewaysHours(time.Now(), 4, gateways); err != nil {
 				common.Logger.Errorf("Failed to collect gateway hourly stats: %v", err)
+			}
+
+			if err := refreshContinuousAggregate(time.Now()); err != nil {
+				common.Logger.Errorf("Failed to refresh continuous aggregates: %v", err)
 			}
 		}
 	}()
 }
 
-// CollectGatewayHourlyStats collects gateway hourly stats at 5 minutes past each hour
-// Gets data for previous hours and stores in database
-// hoursAgo specifies how many previous hours to collect, defaults to 4 if not specified
-func CollectGatewayHourlyStats(hoursAgo ...int) error {
-	common.Logger.Infof("Collecting gateway hourly stats")
-	// Get hours to collect, default to 4 if not specified
-	hours := 4
-	if len(hoursAgo) > 0 && hoursAgo[0] > 0 {
-		hours = hoursAgo[0]
+// 手动收集指定日期的数据，开始时间结束时间格式为 2024-01-01
+func ManuCollectGatewayHourlyStatsByDay(start, end string) error {
+	if start == "" || end == "" {
+		return errors.New("Start and end dates are required")
 	}
 
-	// Get current time
-	now := time.Now()
+	// Parse start and end dates
+	startTime, err := time.Parse("2006-01-02", start)
+	if err != nil {
+		return errors.Wrap(err, "Failed to parse start date")
+	}
 
-	// Get all gateways
+	endTime, err := time.Parse("2006-01-02", end)
+	if err != nil {
+		return errors.Wrap(err, "Failed to parse end date")
+	}
+
+	// Validate dates
+	if endTime.Before(startTime) {
+		return errors.New("End date must be after start date")
+	}
+
 	gateways, err := GetAllEcgateways()
 	if err != nil {
-		return err
+		return errors.Wrap(err, "Failed to get gateways")
+	}
+
+	if len(gateways) == 0 {
+		return errors.New("No gateways found")
+	}
+
+	// Iterate through each day
+	for currentDate := startTime; !currentDate.After(endTime); currentDate = currentDate.AddDate(0, 0, 1) {
+		if err := collectGatewaysFullDay(currentDate, gateways); err != nil {
+			common.Logger.Errorf("Failed to collect stats for %s: %v",
+				currentDate.Format("2006-01-02"), err)
+			return err
+		}
+
+		if err := refreshContinuousAggregate(currentDate); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func collectGatewaysFullDay(collectTime time.Time, gateways []model.Ecgateway) error {
+	// Group gateways by project code
+	projectGateways := make(map[string][]model.Ecgateway)
+	for _, gateway := range gateways {
+		projectCode := gateway.ProjectCode
+		if len(projectCode) == 0 {
+			var err error
+			projectCode, err = client.GetBoxProjectCode(gateway.MacAddr)
+			if err != nil {
+				return errors.Wrapf(err, "Failed to get project code for gateway %s", gateway.ID)
+			}
+			gateway.ProjectCode = projectCode
+			if err := common.DbUpsert[model.Ecgateway](context.Background(), common.GetDaprClient(), gateway, model.EcgatewayTableInfo.Name, model.Ecgateway_FIELD_NAME_id); err != nil {
+				return errors.Wrapf(err, "Failed to update project code for gateway %s", gateway.ID)
+			}
+		}
+		projectGateways[projectCode] = append(projectGateways[projectCode], gateway)
+	}
+
+	// For each project, collect full day stats
+	for projectCode, projectGateways := range projectGateways {
+		// Process gateways in batches of 20
+		for i := 0; i < len(projectGateways); i += 20 {
+			end := i + 20
+			if end > len(projectGateways) {
+				end = len(projectGateways)
+			}
+			
+			gatewayBatch := projectGateways[i:end]
+			macAddrs := make([]string, len(gatewayBatch))
+			for j, gateway := range gatewayBatch {
+				macAddrs[j] = gateway.MacAddr
+			}
+
+			reqBody := map[string]string{
+				"projectCode": projectCode,
+				"mac":         strings.Join(macAddrs, ","),
+				"year":        collectTime.Format("2006"),
+				"month":       collectTime.Format("01"),
+				"day":         collectTime.Format("02"),
+			}
+
+			respBytes, err := client.GetBoxesHourStats(reqBody)
+			if err != nil {
+				return errors.Wrap(err, "Failed to get box hour stats")
+			}
+
+			var resp struct {
+				Code    string                 `json:"code"`
+				Message string                 `json:"message"`
+				Data    map[string]interface{} `json:"data"`
+			}
+
+			if err := json.Unmarshal(respBytes, &resp); err != nil {
+				return errors.Wrap(err, "Failed to unmarshal response")
+			}
+
+			if resp.Code != "0" {
+				return fmt.Errorf("API error: %s", resp.Message)
+			}
+
+			// Process response for each gateway and hour
+			for _, gateway := range gatewayBatch {
+				var hourlyStats []model.Eco_gateway_1h
+
+				if gatewayData, ok := resp.Data[gateway.MacAddr].(map[string]interface{}); ok {
+					for hour := 0; hour < 24; hour++ {
+						hourStr := fmt.Sprintf("%02d", hour)
+						if hourData, ok := gatewayData[hourStr].([]interface{}); ok {
+							stats := processHourStats(hourData)
+							hourTime := time.Date(collectTime.Year(), collectTime.Month(), collectTime.Day(), hour, 0, 0, 0, collectTime.Location())
+							
+							stat := model.Eco_gateway_1h{
+								ID:               gateway.ID + "_" + hourTime.Format("2006010215"),
+								Time:             common.LocalTime(hourTime),
+								GatewayID:        gateway.ID,
+								FloorID:          gateway.FloorID,
+								BuildingID:       gateway.BuildingID,
+								Type:             gateway.Type,
+								PowerConsumption: getTotalElectricity(stats),
+							}
+							hourlyStats = append(hourlyStats, stat)
+						}
+					}
+				}
+
+				if len(hourlyStats) > 0 {
+					if err := saveGatewayHourlyStats(hourlyStats); err != nil {
+						return errors.Wrap(err, "Failed to save gateway hourly stats")
+					}
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func collectGatewaysHours(collectTime time.Time, hoursAgo int, gateways []model.Ecgateway) error {
+	if hoursAgo <= 0 {
+		return errors.New("hoursAgo must be greater than 0")
 	}
 
 	// Group gateways by project code
@@ -65,102 +206,152 @@ func CollectGatewayHourlyStats(hoursAgo ...int) error {
 	for _, gateway := range gateways {
 		projectCode := gateway.ProjectCode
 		if len(projectCode) == 0 {
+			var err error
 			projectCode, err = client.GetBoxProjectCode(gateway.MacAddr)
 			if err != nil {
-				common.Logger.Errorf("Failed to get project code for gateway %s: %v", gateway.ID, err)
-				return errors.Wrap(err, "Failed to get project code for gateway")
+				return errors.Wrapf(err, "Failed to get project code for gateway %s", gateway.ID)
 			}
 			gateway.ProjectCode = projectCode
-			err = common.DbUpsert[model.Ecgateway](context.Background(), common.GetDaprClient(), gateway, model.EcgatewayTableInfo.Name, model.Ecgateway_FIELD_NAME_id)
-			if err != nil {
-				common.Logger.Errorf("Failed to update project code for gateway %s: %v", gateway.ID, err)
-				return errors.Wrap(err, "Failed to update project code for gateway")
+			if err := common.DbUpsert[model.Ecgateway](context.Background(), common.GetDaprClient(), gateway, model.EcgatewayTableInfo.Name, model.Ecgateway_FIELD_NAME_id); err != nil {
+				return errors.Wrapf(err, "Failed to update project code for gateway %s", gateway.ID)
 			}
 		}
-
 		projectGateways[projectCode] = append(projectGateways[projectCode], gateway)
 	}
 
-	// For each project, collect stats for all gateways
-	for projectCode, gateways := range projectGateways {
-		// Get mac addresses for all gateways in this project
-		macAddrs := make([]string, len(gateways))
-		for i, gateway := range gateways {
-			macAddrs[i] = gateway.MacAddr
-		}
-
-		// Collect stats for previous hours
-		for i := 1; i <= hours; i++ {
-			// Calculate hour timestamp
-			hourTime := now.Add(time.Duration(-i) * time.Hour)
-			hourTime = time.Date(hourTime.Year(), hourTime.Month(), hourTime.Day(),
-				hourTime.Hour(), 0, 0, 0, hourTime.Location())
-
-			// Call GET_BOXES_HOUR_STATS API with all mac addresses
-			reqBody := map[string]string{
-				"projectCode": projectCode,
-				"mac":         strings.Join(macAddrs, ","),
-				"year":        hourTime.Format("2006"),
-				"month":       hourTime.Format("01"),
-				"day":         hourTime.Format("02"),
-				"hour":        hourTime.Format("15"),
+	// For each project, collect stats for specified hours
+	for projectCode, projectGateways := range projectGateways {
+		// Process gateways in batches of 20
+		for i := 0; i < len(projectGateways); i += 20 {
+			end := i + 20
+			if end > len(projectGateways) {
+				end = len(projectGateways)
+			}
+			
+			gatewayBatch := projectGateways[i:end]
+			macAddrs := make([]string, len(gatewayBatch))
+			for j, gateway := range gatewayBatch {
+				macAddrs[j] = gateway.MacAddr
 			}
 
-			respBytes, err := client.GetBoxesHourStats(reqBody)
-			if err != nil {
-				return err
-			}
+			for i := 1; i <= hoursAgo; i++ {
+				hourTime := collectTime.Add(time.Duration(-i) * time.Hour)
+				hourTime = time.Date(hourTime.Year(), hourTime.Month(), hourTime.Day(),
+					hourTime.Hour(), 0, 0, 0, hourTime.Location())
 
-			var resp struct {
-				Code    string `json:"code"`
-				Message string `json:"message"`
-				Data    map[string][]struct {
-					Addr        int     `json:"addr"`
-					Electricity float64 `json:"electricity"`
-				} `json:"data"`
-			}
-
-			if err := json.Unmarshal(respBytes, &resp); err != nil {
-				return err
-			}
-
-			if resp.Code != "0" {
-				return fmt.Errorf("API error: %s", resp.Message)
-			}
-
-			// Process response for each gateway
-			for _, gateway := range gateways {
-				hourlyStats := model.Eco_gateway_1h{
-					ID:               gateway.ID + "_" + hourTime.Format("2006010215"),
-					Time:             common.LocalTime(hourTime),
-					GatewayID:        gateway.ID,
-					FloorID:          gateway.FloorID,
-					BuildingID:       gateway.BuildingID,
-					Type:             gateway.Type,
-					PowerConsumption: 0,
+				reqBody := map[string]string{
+					"projectCode": projectCode,
+					"mac":         strings.Join(macAddrs, ","),
+					"year":        hourTime.Format("2006"),
+					"month":       hourTime.Format("01"),
+					"day":         hourTime.Format("02"),
+					"hour":        hourTime.Format("15"),
 				}
 
-				// Get total electricity from addr 0
-				if stats, ok := resp.Data[gateway.MacAddr]; ok {
-					for _, stat := range stats {
-						if stat.Addr == 0 {
-							hourlyStats.PowerConsumption = stat.Electricity
-							break
+				respBytes, err := client.GetBoxesHourStats(reqBody)
+				if err != nil {
+					return errors.Wrap(err, "Failed to get box hour stats")
+				}
+
+				var resp struct {
+					Code    string                 `json:"code"`
+					Message string                 `json:"message"`
+					Data    map[string]interface{} `json:"data"`
+				}
+
+				if err := json.Unmarshal(respBytes, &resp); err != nil {
+					return errors.Wrap(err, "Failed to unmarshal response")
+				}
+
+				if resp.Code != "0" {
+					return fmt.Errorf("API error: %s", resp.Message)
+				}
+
+				// Process response for each gateway
+				for _, gateway := range gatewayBatch {
+					if statsArr, ok := resp.Data[gateway.MacAddr].([]interface{}); ok {
+						stats := processHourStats(statsArr)
+						hourlyStats := []model.Eco_gateway_1h{{
+							ID:               gateway.ID + "_" + hourTime.Format("2006010215"),
+							Time:             common.LocalTime(hourTime),
+							GatewayID:        gateway.ID,
+							FloorID:          gateway.FloorID,
+							BuildingID:       gateway.BuildingID,
+							Type:             gateway.Type,
+							PowerConsumption: getTotalElectricity(stats),
+						}}
+
+						if err := saveGatewayHourlyStats(hourlyStats); err != nil {
+							return errors.Wrap(err, "Failed to save gateway hourly stats")
 						}
 					}
 				}
-
-				
 			}
 		}
-		
 	}
 
-	// Refresh continuous aggregates based on type
+	return nil
+}
+
+func processHourStats(data []interface{}) []struct {
+	Addr        int     `json:"addr"`
+	Electricity float64 `json:"electricity"`
+} {
+	var stats []struct {
+		Addr        int     `json:"addr"`
+		Electricity float64 `json:"electricity"`
+	}
+
+	for _, stat := range data {
+		if statMap, ok := stat.(map[string]interface{}); ok {
+			addr, ok1 := statMap["addr"].(float64)
+			electricity, ok2 := statMap["electricity"].(float64)
+			if !ok1 || !ok2 {
+				continue
+			}
+			stats = append(stats, struct {
+				Addr        int     `json:"addr"`
+				Electricity float64 `json:"electricity"`
+			}{
+				Addr:        int(addr),
+				Electricity: electricity,
+			})
+		}
+	}
+
+	return stats
+}
+
+func getTotalElectricity(stats []struct {
+	Addr        int     `json:"addr"`
+	Electricity float64 `json:"electricity"`
+}) float64 {
+	for _, stat := range stats {
+		if stat.Addr == 0 {
+			return stat.Electricity
+		}
+	}
+	return 0
+}
+
+func saveGatewayHourlyStats(stats []model.Eco_gateway_1h) error {
+	if len(stats) == 0 {
+		return nil
+	}
+
+	err := common.DbBatchUpsert(context.Background(), common.GetDaprClient(), stats, model.Eco_gateway_1hTableInfo.Name, model.Eco_gateway_1h_FIELD_NAME_id)
+	if err != nil {
+		return errors.Wrap(err, "Failed to batch upsert gateway hourly stats")
+	}
+	
+	return nil
+}
+
+func refreshContinuousAggregate(collectTime time.Time) error {
 	for tableName, refreshType := range needRefreshContinuousAggregateMap {
-		startTime := time.Now()
+		startTime := collectTime
 		endTime := startTime
-		
+
 		switch refreshType {
 		case "day":
 			startTime = time.Date(startTime.Year(), startTime.Month(), startTime.Day(), 0, 0, 0, 0, startTime.Location())
@@ -171,21 +362,22 @@ func CollectGatewayHourlyStats(hoursAgo ...int) error {
 		case "year":
 			startTime = time.Date(startTime.Year(), 1, 1, 0, 0, 0, 0, startTime.Location())
 			endTime = startTime.AddDate(1, 0, 0)
+		default:
+			return fmt.Errorf("Invalid refresh type: %s", refreshType)
 		}
 
-		if err := common.DbRefreshContinuousAggregate(context.Background(), common.GetDaprClient(), 
+		if err := common.DbRefreshContinuousAggregate(context.Background(), common.GetDaprClient(),
 			tableName, startTime.Format("2006-01-02"), endTime.Format("2006-01-02")); err != nil {
-			return err
+			return errors.Wrapf(err, "Failed to refresh continuous aggregate for table %s", tableName)
 		}
 	}
-
 	return nil
 }
 
 func GetAllEcgateways() ([]model.Ecgateway, error) {
 	datas, err := common.DbQuery[model.Ecgateway](context.Background(), common.GetDaprClient(), model.EcgatewayTableInfo.Name, "")
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "Failed to query gateways")
 	}
 	return datas, nil
 }
@@ -193,7 +385,7 @@ func GetAllEcgateways() ([]model.Ecgateway, error) {
 func GetAllEcbuildings() ([]model.Ecbuilding, error) {
 	datas, err := common.DbQuery[model.Ecbuilding](context.Background(), common.GetDaprClient(), model.EcbuildingTableInfo.Name, "")
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "Failed to query buildings")
 	}
 	return datas, nil
 }
