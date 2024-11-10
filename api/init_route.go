03b7d6ec67da27d9329b05af5fc74bec058a282a
lyl <liuyulong@defensedriver.com.cn>
2024-11-10 18:37:26 +0800

    删除：     api/eco_building_info_1d.go

--- api/init_route.go
+++ api/init_route.go
@@ -3,11 +3,10 @@ package api
 import "github.com/go-chi/chi/v5"
 
 func InitRoute(r chi.Router) {
-	InitEco_building_info_1dRoute(r)
-	InitEco_building_info_1mRoute(r)
-	InitEco_building_info_1yRoute(r)
 	InitEcgatewayRoute(r)
 	InitEcfloorRoute(r)
 	InitEcbuildingRoute(r)
+	InitEcparkRoute(r)
 	InitManuCollectRoute(r)
+	InitDashboardRoute(r)
 }
