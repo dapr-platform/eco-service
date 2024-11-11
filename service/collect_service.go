e933f8860dfd465779220d5c7a520ceb57212de6
lyl <liuyulong@defensedriver.com.cn>
2024-11-11 13:31:18 +0800

    修改：     service/collect_service.go

--- service/collect_service.go
+++ service/collect_service.go
@@ -5,7 +5,6 @@ import (
 	"crypto/md5"
 	"eco-service/client"
 	"eco-service/model"
-	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"strings"
@@ -126,7 +125,7 @@ func demoWaterDataGenHourly(startTime time.Time) {
 
 	// 构造数据
 	waterData := model.Eco_park_water_1h{
-		ID:               fmt.Sprintf("%x", md5.Sum([]byte(park.ID + "_" + lastHour.Format("2006010215")))),
+		ID:               fmt.Sprintf("%x", md5.Sum([]byte(park.ID+"_"+lastHour.Format("2006010215")))),
 		Time:             common.LocalTime(lastHour),
 		ParkID:           park.ID,
 		WaterConsumption: waterConsumption,
